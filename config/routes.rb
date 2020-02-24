3ed3bdd53fdd8b568bad8c1b8d0b06875aa338ce
Timothee Boingneres <timboing@icloud.com>
2020-02-24 18:09:08 +0100

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :game_sessions, only: [:new, :create, :show], shallow: true do
+    resources :rounds, only: [:index, :new, :create, :show], shallow: true do
+      resources :round_participation, only: [:new, :create, :edit, :update]
+    end
+  end
 
 end
