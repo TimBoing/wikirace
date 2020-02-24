212bc4034549a81a263afda571ea013b2a193d9c
Timothee Boingneres <timboing@icloud.com>
2020-02-24 16:57:42 +0100

    basic associations in place

--- app/models/game_session.rb
+++ app/models/game_session.rb
@@ -1,3 +1,4 @@
 class GameSession < ApplicationRecord
   belongs_to :user
+  has_many :rounds
 end
--- app/models/round_participation.rb
+++ app/models/round_participation.rb
@@ -1,4 +1,5 @@
 class RoundParticipation < ApplicationRecord
   belongs_to :user
   belongs_to :round
+  has_many :visited_pages
 end
