15e386d4a7f04c00b282bfcf6521be51f73d8c5d
filia alexis <filia.alexis@gmail.com>
2020-02-25 17:30:36 +0100

    Redirect vers la bonne page quand on est pas le createur

--- app/controllers/round_participations_controller.rb
+++ app/controllers/round_participations_controller.rb
@@ -1,10 +1,14 @@
 class RoundParticipationsController < ApplicationController
   def new
     @round_participation = RoundParticipation.new
+    @round = Round.find(params[:round_id])
+    @game_session = GameSession.find(@round.game_session_id)
   end
 
   def create
     @round_participation = RoundParticipation.new(params[:user])
+    @round = Round.find(params[:round_id])
+    @game_session = GameSession.find(@round.game_session_id)
     render 'game_sessions/show'
   end
 
