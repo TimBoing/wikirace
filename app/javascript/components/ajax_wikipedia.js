9596ee191283b5a38f0f48ad0ff1a93aceaee612
Timothee Boingneres <timboing@icloud.com>
2020-02-25 17:50:17 +0100

    les liens morts repérés grace a leur classe new

--- app/javascript/components/ajax_wikipedia.js
+++ app/javascript/components/ajax_wikipedia.js
@@ -1,11 +1,14 @@
 const requestAndDisplay = () => {
-  const page_name = 'poule';
+  const page_name = 'Faculté_de_théologie_protestante_de_Strasbourg';
   const pageContainer = document.getElementById('test-container-for-receiving-ajax');
+  let previousPage;
 
    const requestWikipageContent = (page) => {
+    previousPage = page;
+    console.log(previousPage);
     const requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
+      method: 'GET',
+      redirect: 'follow'
     };
     fetch(`https://fr.wikipedia.org/api/rest_v1/page/html/${page}`, requestOptions)
       .then(response => response.text())
@@ -16,13 +19,18 @@ const requestAndDisplay = () => {
   const addEventsOnWikiLinks = () => {
     const wikiLinks = pageContainer.querySelectorAll('a');
     wikiLinks.forEach((link) => {
-      console.log('adding event');
       link.addEventListener('click', (event)=> {
         event.preventDefault();
-        const linkRef = event.currentTarget.getAttribute("href");
-        const stripedRef = linkRef.substring(2);
-        pageContainer.innerHTML = '';
-        requestWikipageContent(stripedRef);
+
+        if(event.currentTarget.classList.contains('new')){
+          alert('ce lien est mort')
+        }else{
+          const linkRef = event.currentTarget.getAttribute("href");
+          const stripedRef = linkRef.substring(2);
+          pageContainer.innerHTML = '';
+          requestWikipageContent(stripedRef);
+        }
+
       });
     });
   }
@@ -42,3 +50,4 @@ export { requestAndDisplay };
 // event.currentTarget.getAttribute("href") will give you ./endofurl
 // event.currentTarget.hrf will give you the full url
 // str.substring(indiceA[, indiceB]) ==> start and end
+// les liens morts ont une class "new"
