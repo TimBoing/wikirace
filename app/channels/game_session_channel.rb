7aecc5e83d6b0705072798f2f0f928797ae54c09
maxime vanderwees <vanderwees.maxime@gmail.com>
2020-02-26 13:05:19 +0100

    chat nearly working

--- app/channels/game_session_channel.rb
+++ app/channels/game_session_channel.rb
@@ -1,6 +1,7 @@
 class GameSessionChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "game_session_channel#{params[:game_session_id]}"
+    raise
+    stream_from "game_session_channel_#{params[:game_session_id]}"
   end
 
   def unsubscribed
