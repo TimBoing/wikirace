540044675f507d683049eeb320f47a3c04c9a49e
filia alexis <filia.alexis@gmail.com>
2020-02-26 12:40:25 +0100

    Ajouter le save dans round pareticipation

--- app/controllers/round_participations_controller.rb
+++ app/controllers/round_participations_controller.rb
@@ -13,11 +13,9 @@ class RoundParticipationsController < ApplicationController
     else
       @round = Round.find(round_participations_params[:round_id])
     end
+    @round_participation.save
     @game_session = GameSession.find(@round.game_session_id)
     render 'game_sessions/show'
-
-    # Attention lors de la creation de nouvelles instances de RoundParticipation,
-    # on les sauve pas dans la DB... on les sauvera a la fin du jeu (Update)?
   end
 
   def edit
