1b743b8ba4a2cc8b3ba9d6822d11fadb80dd88a6
Timothee Boingneres <timboing@icloud.com>
2020-02-26 13:01:58 +0100

    random page for start and end easy pizzy

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'open-uri'
+
 class RoundsController < ApplicationController
   def index
     @game_session = GameSession.find(params[:game_session_id])
@@ -28,6 +31,20 @@ class RoundsController < ApplicationController
   def create
     @round = Round.new(round_params)
     @round.game_session_id = params[:game_session_id].to_i
+
+    # Creation of the random start page and end page through API----
+    url_for_random_title = 'https://fr.wikipedia.org/api/rest_v1/page/random/title'
+    start_page_raw = open(url_for_random_title).read
+    start_page_json = JSON.parse(start_page_raw)
+    start_page = start_page_json["items"][0]['title']
+    end_page_raw = open(url_for_random_title).read
+    end_page_json = JSON.parse(end_page_raw)
+    end_page = end_page_json["items"][0]['title']
+
+    @round.start_page = start_page
+    @round.end_page = end_page
+    # End of creation-----------------------------------------------
+
     if @round.save
       round_participation = RoundParticipation.new
       round_participation.user_id = current_user.id
