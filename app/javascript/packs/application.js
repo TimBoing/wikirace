80ab4da9599540af1051ef8a8de4c4281503b319
Geoffrey Dulac <geoffrey.dulac2@gmail.com>
2020-02-27 18:19:18 +0100

    V1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,12 @@ import { requestAndDisplay } from '../components/ajax_wikipedia'
 import { trackMouse } from '../components/mouseTrack';
 import { joinRound } from '../components/join_round'
 import { toggleNavbar } from '../components/toggle_navbar';
+import '../components/copy_url'
 
 requestAndDisplay();
 toggleNavbar();
 joinRound();
+
+
 
 //trackMouse();
