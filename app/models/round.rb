b1a4cbb21eff5a86ac14aa85793901c5b8655039
filia alexis <filia.alexis@gmail.com>
2020-02-28 17:43:25 +0100

    Le tableau des score de la Game session marche

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,5 +1,6 @@
 class Round < ApplicationRecord
   belongs_to :game_session
   has_many :round_participations
+  has_many :users, through: :round_participations
   validates :game_session, presence: true
 end
