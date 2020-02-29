21e97ac4bb752e749b3952ec69724ac88d6bc237
Timothee Boingneres <timboing@icloud.com>
2020-02-29 18:49:53 +0100

    type js added

---
+++ app/javascript/components/banner_typed.js
@@ -0,0 +1,12 @@
+import Typed from 'typed.js';
+
+const loadDynamicBannerText = () => {
+  const title = document.getElementById('#home-page-title');
+  new Typed(title, {
+    strings: ["Bienvenue sur", "WikiRACE!"],
+    typeSpeed: 80,
+    loop: true
+  });
+}
+
+export { loadDynamicBannerText };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
 import "bootstrap";
+import { loadDynamicBannerText } from '../components/banner_typed';
 import { startGame } from '../components/start_game';
 import { initGame } from '../components/game_controller';
 import { trackMouse } from '../components/mouseTrack';
@@ -7,6 +8,7 @@ import { toggleNavbar } from '../components/toggle_navbar';
 import '../components/copy_url'
 import { handleGameModals } from '../components/modals_game';
 
+loadDynamicBannerText();
 startGame();
 initGame();
 toggleNavbar();
