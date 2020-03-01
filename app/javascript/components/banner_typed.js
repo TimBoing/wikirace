47a2cac481c5ef7e555e7f2976236e3f009b2a95
Timothee Boingneres <timboing@icloud.com>
2020-03-01 11:22:16 +0100

    allez je pars sur matrix

--- app/javascript/components/banner_typed.js
+++ app/javascript/components/banner_typed.js
@@ -2,11 +2,14 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   const title = document.getElementById('#home-page-title');
-  new Typed(title, {
-    strings: ["Bienvenue","sur", "WikiRACE", "!!!"],
-    typeSpeed: 80,
-    loop: true
-  });
+  if(title){
+    new Typed(title, {
+      strings: ["Enter","the", "WikiRACE", "!!!"],
+      typeSpeed: 80,
+      loop: true
+    });
+  }
+
 }
 
 export { loadDynamicBannerText };
