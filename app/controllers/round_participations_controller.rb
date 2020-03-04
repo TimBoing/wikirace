2309fa04c306c161346cc28555d0aa552d64ec61
maxime vanderwees <vanderwees.maxime@gmail.com>
2020-03-04 16:15:10 +0100

    invit friends modal deisgn

--- app/controllers/round_participations_controller.rb
+++ app/controllers/round_participations_controller.rb
@@ -19,11 +19,7 @@ class RoundParticipationsController < ApplicationController
     if @round_participation.is_the_best?
       @round_participation.save_record(current_user)
     end
-    # @round_participations.each{ |round_participation|
-    #   round_participation.rank = round_participation.rank_for_round
-    #   round_participation.score = round_participation.score_for_round
-    #   round_participation.save
-    # }
+
     @sorted_round_participations = @round_participations.sort_by{ |round_participation| round_participation.rank}
     @players = @round_participations.map { |round_participation| round_participation.user }
   end
