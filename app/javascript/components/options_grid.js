ab015351dd628ac9351935ec1b1b7bd40b7710c4
Timothee Boingneres <timboing@icloud.com>
2020-03-05 20:28:29 +0100

    ok sur le seachbar desactivation

--- app/javascript/components/options_grid.js
+++ app/javascript/components/options_grid.js
@@ -1,18 +1,42 @@
 const optionsSelection = () => {
-  const options = document.querySelectorAll(".selectable-option");
 
-  if(options){
+  const searchBar = document.getElementById("round_search_bar");
+  const reverse = document.getElementById("round_reverse");
+  const searchBarOption = document.getElementById("grid-option-search-bar");
+  const reverseOption = document.getElementById("grid-option-reverse");
 
-    options.forEach((option) => {
+  const toggleActive = (element) => {
+    element.classList.toggle("active");
+  }
 
-      option.addEventListener('click', (event) => {
-        option.classList.toggle("active");
-        console.log("Im here");
-      });
 
+  if(searchBar){
+
+
+    searchBarOption.addEventListener('click', (event) => {
+      toggleActive(searchBarOption);
+      if(searchBar.checked === true){
+        searchBar.checked = false;
+        console.log("changing value to 0");
+      } else {
+        searchBar.checked = true;
+        console.log("changing value to 1");
+      }
 
     });
 
+
+    reverseOption.addEventListener('click', (event) => {
+      toggleActive(reverseOption);
+      if(reverse.checked === true){
+        reverse.checked = false;
+        console.log("changing value to 0");
+      } else {
+        reverse.checked = true;
+        console.log("changing value to 1");
+      }
+
+    });
 
   }
 
@@ -20,3 +44,7 @@ const optionsSelection = () => {
 }
 
 export{optionsSelection};
+
+
+
+
